f8ee4b6471f40b085019327f3a46aab89a85641b
Belle <bellesee1212@gmail.com>
2025-01-22 14:45:18 -0500

    hello

--- pages/Aggregate_Data_Analytics.py
+++ pages/Aggregate_Data_Analytics.py
@@ -22,75 +22,75 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-col = st.columns((5.5, 4.5), gap='medium')
-with col[0]:
-    st.title("Total: 14,330 Domains")
-    # st.metric("Domains covered", 14330)
-    c1, c2, c3 = st.columns(3)
-    with c1:
-        st.metric("Domains on Whois", "100%")
-    with c2:
-        st.metric("Domains on Wayback", "99%")
-    with c3:
-        st.metric("Domains on Wikidata", "25%")
-# with col[1]:
-#     st.metric("Domains on Wayback", "99%")
-#     st.metric("Domains on Wikidata", "25%")
-with col[1]:
-    st.title("Quick Insights")
-    st.text("- Older domains tend to have more snapshots")
-    st.text("- The most common behind-the scene organization is businesses. Other common types include newspaper, nonprofit, and educational institutions")
-
-combined_df, wayback_df, wikidata_df, whois_df = get_combined_df()
-
-col = st.columns((3.5, 1.5, 1.5))
-st.subheader("Histogram of Snapshots on Wayback")
-log = st.toggle("Log scale", value=True)
-if log:
-    st.caption("Note: the log scale is applied to help visualize against outliers.")
-    st.plotly_chart(num_snapshots_histogram(combined_df, nbins=80, log_scale=True))
-else:
-    st.plotly_chart(num_snapshots_histogram(combined_df, nbins=80))
+# col = st.columns((5.5, 4.5), gap='medium')
 # with col[0]:
+#     st.title("Total: 14,330 Domains")
+#     # st.metric("Domains covered", 14330)
+#     c1, c2, c3 = st.columns(3)
+#     with c1:
+#         st.metric("Domains on Whois", "100%")
+#     with c2:
+#         st.metric("Domains on Wayback", "99%")
+#     with c3:
+#         st.metric("Domains on Wikidata", "25%")
+# # with col[1]:
+# #     st.metric("Domains on Wayback", "99%")
+# #     st.metric("Domains on Wikidata", "25%")
+# with col[1]:
+#     st.title("Quick Insights")
+#     st.text("- Older domains tend to have more snapshots")
+#     st.text("- The most common behind-the scene organization is businesses. Other common types include newspaper, nonprofit, and educational institutions")
+
+# combined_df, wayback_df, wikidata_df, whois_df = get_combined_df()
+
+# col = st.columns((3.5, 1.5, 1.5))
+# st.subheader("Histogram of Snapshots on Wayback")
+# log = st.toggle("Log scale", value=True)
+# if log:
+#     st.caption("Note: the log scale is applied to help visualize against outliers.")
+#     st.plotly_chart(num_snapshots_histogram(combined_df, nbins=80, log_scale=True))
+# else:
 #     st.plotly_chart(num_snapshots_histogram(combined_df, nbins=80))
-    # st.plotly_chart(swarm_plot(combined_df))
+# # with col[0]:
+# #     st.plotly_chart(num_snapshots_histogram(combined_df, nbins=80))
+#     # st.plotly_chart(swarm_plot(combined_df))
 
-mean_snapshots, median_snapshots, std_snapshots, top_domains, minimum, maximum= get_stats(wayback_df, "Num_Snapshots")
-# st.text(mean_snapshots)
-cols = st.columns((1.5,1.5,1.5, 3), gap='medium')
-with cols[0]:
-    st.metric(label="Average Number of Snapshots", value=f"{round(mean_snapshots):,}")
-    st.metric(label="Minimum Number of Snapshots", value=f"{round(minimum):,}")
-with cols[1]:
-    st.metric(label="Median of Snapshots", value=f"{round(median_snapshots):,}")
-    st.metric(label="Maximum Number of Snapshots", value=f"{round(maximum):,}")
-with cols[2]:
-    st.metric(label="Standard Deviation of Snapshots", value=f"{round(std_snapshots):,}")
-with cols[3]:
-    st.subheader("Domains with Top 3 Most Snapshots")
-    st.table(top_domains[['Domain', 'Num_Snapshots']].reset_index(drop=True).style.format({"Num_Snapshots": "{:,}"}))
+# mean_snapshots, median_snapshots, std_snapshots, top_domains, minimum, maximum= get_stats(wayback_df, "Num_Snapshots")
+# # st.text(mean_snapshots)
+# cols = st.columns((1.5,1.5,1.5, 3), gap='medium')
+# with cols[0]:
+#     st.metric(label="Average Number of Snapshots", value=f"{round(mean_snapshots):,}")
+#     st.metric(label="Minimum Number of Snapshots", value=f"{round(minimum):,}")
+# with cols[1]:
+#     st.metric(label="Median of Snapshots", value=f"{round(median_snapshots):,}")
+#     st.metric(label="Maximum Number of Snapshots", value=f"{round(maximum):,}")
+# with cols[2]:
+#     st.metric(label="Standard Deviation of Snapshots", value=f"{round(std_snapshots):,}")
+# with cols[3]:
+#     st.subheader("Domains with Top 3 Most Snapshots")
+#     st.table(top_domains[['Domain', 'Num_Snapshots']].reset_index(drop=True).style.format({"Num_Snapshots": "{:,}"}))
 
-st.divider()
-st.subheader("Distribution of Domain Categories")
-st.plotly_chart(category_bar(wikidata_df))
+# st.divider()
+# st.subheader("Distribution of Domain Categories")
+# st.plotly_chart(category_bar(wikidata_df))
 
 
 
-cols = st.columns((2.5,2), gap='medium')
-with cols[0]:
-    st.subheader("Number of Snapshots Over Age")
-    t1,t2 = st.tabs(["Closer View", "Full View"])
+# cols = st.columns((2.5,2), gap='medium')
+# with cols[0]:
+#     st.subheader("Number of Snapshots Over Age")
+#     t1,t2 = st.tabs(["Closer View", "Full View"])
 
-    with t1:
-        st.text("As age increases, number of snapshots on Wayback tends to increase.")
-        st.image("images/age_snapshot_plot.png")
+#     with t1:
+#         st.text("As age increases, number of snapshots on Wayback tends to increase.")
+#         st.image("images/age_snapshot_plot.png")
 
-    with t2:
-        st.text("Zooming out to see outliers. Well-known online platforms have exceptionally large numbers of snapshots captured on Wayback. ")
-        st.image("images/age_snapshot_zoom.png")
+#     with t2:
+#         st.text("Zooming out to see outliers. Well-known online platforms have exceptionally large numbers of snapshots captured on Wayback. ")
+#         st.image("images/age_snapshot_zoom.png")
 
-with cols[1]:
-    st.subheader("Distribution of Domain Registration Year")
-    st.plotly_chart(date_bar(combined_df))
+# with cols[1]:
+#     st.subheader("Distribution of Domain Registration Year")
+#     st.plotly_chart(date_bar(combined_df))
 
 
--- pages/Single_Domain_Explorer.py
+++ pages/Single_Domain_Explorer.py
@@ -10,110 +10,110 @@ st.set_page_config(layout="wide")
 st.subheader('Wellesley Cred Lab')
 st.title('Web Credibility Explorer')
 
-domain = st.text_input("Enter website URL:", "nytimes.com", key="no_auto_cap", help="Input text without capitalization")
-st.text('Note: please enter url without prefix, e.g. nytimes.com')
+# domain = st.text_input("Enter website URL:", "nytimes.com", key="no_auto_cap", help="Input text without capitalization")
+# st.text('Note: please enter url without prefix, e.g. nytimes.com')
 
-combined_df,_,_,_ = get_combined_df()
+# combined_df,_,_,_ = get_combined_df()
 
-if domain not in combined_df['Domain'].to_list():
-    st.header("Sorry! This domain was not covered by our project.")
-else:
+# if domain not in combined_df['Domain'].to_list():
+#     st.header("Sorry! This domain was not covered by our project.")
+# else:
         
-    domain_row = combined_df[combined_df['Domain']==domain]
+#     domain_row = combined_df[combined_df['Domain']==domain]
 
-    st.divider()
+#     st.divider()
 
 
-    c1,c2,c3 = st.columns(3)
+#     c1,c2,c3 = st.columns(3)
 
-    with c1:
-        date_obj = datetime.strptime(domain_row['registration date'].values[0], "%Y/%m/%d")
+#     with c1:
+#         date_obj = datetime.strptime(domain_row['registration date'].values[0], "%Y/%m/%d")
 
-        st.header(date_obj.strftime("%b %d, %Y"))
-        st.caption("Date Registered")
+#         st.header(date_obj.strftime("%b %d, %Y"))
+#         st.caption("Date Registered")
 
-    with c2:
-        st.header(domain_row['registrar'].values[0])
-        st.caption("Registrar")
+#     with c2:
+#         st.header(domain_row['registrar'].values[0])
+#         st.caption("Registrar")
 
-    with c3:
-        st.header(domain_row['Num_Snapshots'].values[0])
-        st.caption("Number of Snapshots as of 2022")
+#     with c3:
+#         st.header(domain_row['Num_Snapshots'].values[0])
+#         st.caption("Number of Snapshots as of 2022")
 
-    exclude = ['Domain', 'Num_Snapshots', 'registrar']
-    available_columns = [col for col in domain_row.columns if domain_row[col].notnull().any() and col not in exclude]
-    # Create tabs for each non-null column
-    tabs = st.tabs(available_columns)
+#     exclude = ['Domain', 'Num_Snapshots', 'registrar']
+#     available_columns = [col for col in domain_row.columns if domain_row[col].notnull().any() and col not in exclude]
+#     # Create tabs for each non-null column
+#     tabs = st.tabs(available_columns)
 
-    # Display the data for the selected column in each tab
-    for tab, column in zip(tabs, available_columns):
-        with tab:
-            # st.subheader(f"{column} for {domain}")
-            if isinstance(domain_row[column].values[0], list):
-                for item in domain_row[column].values[0]:
-                    st.text(item)
-            elif is_valid_datetime(domain_row[column].values[0]):
-                date_obj = datetime.strptime(domain_row[column].values[0], "%Y/%m/%d")
-                st.subheader(date_obj.strftime("%b %d, %Y"))
-            else:
-                st.header(domain_row[column].values[0])
+#     # Display the data for the selected column in each tab
+#     for tab, column in zip(tabs, available_columns):
+#         with tab:
+#             # st.subheader(f"{column} for {domain}")
+#             if isinstance(domain_row[column].values[0], list):
+#                 for item in domain_row[column].values[0]:
+#                     st.text(item)
+#             elif is_valid_datetime(domain_row[column].values[0]):
+#                 date_obj = datetime.strptime(domain_row[column].values[0], "%Y/%m/%d")
+#                 st.subheader(date_obj.strftime("%b %d, %Y"))
+#             else:
+#                 st.header(domain_row[column].values[0])
     
-    # TODO: add safety net
-    st.subheader("How this domain compares with similar domains")
+#     # TODO: add safety net
+#     st.subheader("How this domain compares with similar domains")
 
-    st.subheader("domains in similar castegories:")
-    similar_domains_df, instance = get_same_category(combined_df, domain)
-    # st.dataframe(similar_domains_df[['Domain']], height=300, width=600)
-    c1,c2 = st.columns(2)
+#     st.subheader("domains in similar castegories:")
+#     similar_domains_df, instance = get_same_category(combined_df, domain)
+#     # st.dataframe(similar_domains_df[['Domain']], height=300, width=600)
+#     c1,c2 = st.columns(2)
 
-    with c1:
-        st.dataframe(similar_domains_df[['Domain']], height=300, width=600)
-    with c2:
-        hist_fig = num_snapshots_histogram(similar_domains_df, instance, domain = domain)
+#     with c1:
+#         st.dataframe(similar_domains_df[['Domain']], height=300, width=600)
+#     with c2:
+#         hist_fig = num_snapshots_histogram(similar_domains_df, instance, domain = domain)
 
-        st.plotly_chart(hist_fig)
-
-
-
-    # click_data = st.plotly_chart(hist_fig, use_container_width=True, key="histogram")
-
-    #     # Get the click data through Streamlit's session state or slider
-    # clicked_bin = st.slider("Select Snapshot Range", min_value=0, max_value=50, step=5)
-
-    # # Handle the bin selection (simulating click)
-    # if clicked_bin is not None:
-    #     # Find the range of domains that fall into this bin
-    #     bins = pd.cut(similar_domains_df['Num_Snapshots'], bins=5)
-    #     domains_in_bin = similar_domains_df[bins == pd.Interval(left=clicked_bin, right=clicked_bin + 5)]['Domain'].tolist()
-
-    #     # Display domains in the sidebar
-    #     st.sidebar.title(f"Domains in Snapshot Range: {clicked_bin} - {clicked_bin + 5}")
-    #     st.sidebar.write(", ".join(domains_in_bin))
+#         st.plotly_chart(hist_fig)
 
 
-    # st.markdown("""
-    #     <style>
-    #     .big-text {
-    #         font-size: 44px !important;  /* Adjust this value for larger or smaller text */
-    #         font-weight: bold;
-    #         color: lightblue;
-    #         margin: 0;
-    #     }
-    #     </style>
-    # """, unsafe_allow_html=True)
 
-    #     st.text_area(
-    #     "Registered",
-    #     whois_row['registration date'].values[0]
-    # )
-        # st.markdown('<p class="big-text">This is big text!</p>', unsafe_allow_html=True)
+#     # click_data = st.plotly_chart(hist_fig, use_container_width=True, key="histogram")
 
-        # with elements("Registered"):
-        #     mui.Typography("Hello world")
-        #     with mui.Paper(elevation=3, variant="outlined", square=True):
-        #         mui.TextField(
-        #             label="My text input",
-        #             defaultValue="Type here",
-        #             variant="outlined",
-        #         )
-    # snapshot_percentile = calculate_percentile(combined_df, domain)
+#     #     # Get the click data through Streamlit's session state or slider
+#     # clicked_bin = st.slider("Select Snapshot Range", min_value=0, max_value=50, step=5)
+
+#     # # Handle the bin selection (simulating click)
+#     # if clicked_bin is not None:
+#     #     # Find the range of domains that fall into this bin
+#     #     bins = pd.cut(similar_domains_df['Num_Snapshots'], bins=5)
+#     #     domains_in_bin = similar_domains_df[bins == pd.Interval(left=clicked_bin, right=clicked_bin + 5)]['Domain'].tolist()
+
+#     #     # Display domains in the sidebar
+#     #     st.sidebar.title(f"Domains in Snapshot Range: {clicked_bin} - {clicked_bin + 5}")
+#     #     st.sidebar.write(", ".join(domains_in_bin))
+
+
+#     # st.markdown("""
+#     #     <style>
+#     #     .big-text {
+#     #         font-size: 44px !important;  /* Adjust this value for larger or smaller text */
+#     #         font-weight: bold;
+#     #         color: lightblue;
+#     #         margin: 0;
+#     #     }
+#     #     </style>
+#     # """, unsafe_allow_html=True)
+
+#     #     st.text_area(
+#     #     "Registered",
+#     #     whois_row['registration date'].values[0]
+#     # )
+#         # st.markdown('<p class="big-text">This is big text!</p>', unsafe_allow_html=True)
+
+#         # with elements("Registered"):
+#         #     mui.Typography("Hello world")
+#         #     with mui.Paper(elevation=3, variant="outlined", square=True):
+#         #         mui.TextField(
+#         #             label="My text input",
+#         #             defaultValue="Type here",
+#         #             variant="outlined",
+#         #         )
+#     # snapshot_percentile = calculate_percentile(combined_df, domain)
